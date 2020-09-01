6a2ab6fab3a341635137c02fb76a0f06141f7e3e
TarunGunampalli <tarun.gunampalli@gmail.com>
2020-08-31 22:56:47 -0500

    Fixed multiple issues with listing and styling

--- popup.js
+++ popup.js
@@ -1,10 +1,16 @@
-document.addEventListener('DOMContentLoaded', onStart, false);
+if (window.location.pathname == '/popup.html') {
+    document.addEventListener('DOMContentLoaded', onStart, false);
+}
+else if (window.location.pathname == '/notes.html') {
+    document.addEventListener('DOMContentLoaded', initNotesPage, false);
+}
 
-var notes = new Array();
+var notes = ['filler'];
+
 
 function onStart() {
-    if (localStorage.getItem('notesArray') != 'null') {
-        notes = localStorage.getItem('notesArray');
+    if (localStorage.getItem('notesArray') !== null) {
+        notes = JSON.parse(localStorage.getItem('notesArray'));
     }
     var saveButton = document.getElementById('saveButton');
     saveButton.addEventListener('click', saveNote, false);
@@ -14,26 +20,35 @@ function onStart() {
 
 function saveNote() {
     var input = document.getElementById('noteField').value;
-    notes.push(input);
-    localStorage.setItem('notesArray', notes);
+    if (notes[0] == 'filler') {
+        notes.pop();
+    }
+    if (!notes.includes(input)) {
+        notes.push(input);
+    }
+    localStorage.setItem('notesArray', JSON.stringify(notes));
     //printAbove(input, 'listEnd');
 }
 
 function openNotesPage() {
     window.open('notes.html', '_self', false);
-    window.onLoad = function() {
-        var backButton = document.getElementById('backButton');
-        backButton.addEventListener('click', homePage, false);
-    }
-    notes = localStorage.getItem('notesArray');
-    for (var i = 0; i < notes.length; i++) {
-        printAbove(notes[i], 'listEnd');
+}
+
+function initNotesPage() {
+    var clearButton = document.getElementById('clearButton');
+    clearButton.addEventListener('click', clearNotes, false);
+    if  (localStorage.getItem('notesArray') !== null) {
+        notes = JSON.parse(localStorage.getItem('notesArray'));
+        for (var i = 0; i < notes.length; i++) {
+            printList(notes[i]);
+        }
     }
 }
 
-function homePage() {
-    printAbove('run', 'listEnd');
-    window.open('popup.html', '_self', false);
+function clearNotes() {
+    notes = [];
+    localStorage.clear();
+    location.reload();
 }
 
 function printAbove(text, element) {
@@ -42,4 +57,12 @@ function printAbove(text, element) {
     d.appendChild(textNode);
     const beforeElement = document.getElementById(element);
     document.body.insertBefore(d, beforeElement);
+}
+
+function printList(text) {
+    const notes = document.getElementById('notes');
+    const listElement = document.createElement('li');
+    textNode = document.createTextNode(text);
+    listElement.textContent = text;
+    notes.appendChild(listElement);
 }
